16d5216ba73c3025261b765f4e2717e73020fd2b
Arek Flinik <aflinik@gmail.com>
2013-06-25 22:53:35 +0200

    binding for making moves

--- uttt_model.js
+++ uttt_model.js
@@ -9,11 +9,23 @@ function BigBoardViewModel() {
 			row.push(new SmallBoardViewModel());
 		}
 		return row;
-	}
+	};
 	for(var i=0; i<3; i++){
 		self.big_rows.push({big_cols: makeRow()});
 	}
 	
+
+	self.nextMove = 'O';
+	self.switchMove = function() {
+		self.nextMove = self.nextMove == 'O' ? 'X' : 'O';
+	}
+	self.nextSmallBoard = -1;
+
+	self.makeMove = function() {
+		this.state(self.nextMove);
+		self.switchMove();
+	}
+
 }
 
 function SmallBoardViewModel() {
@@ -21,22 +33,22 @@ function SmallBoardViewModel() {
 
 	// board setup
 	self.rows = ko.observableArray()
-	var makeRow = function() {
+	var makeRow = function(index) {
 		var row = [];
 		for(var i =0 ; i<3; i++) {
-			row.push(new FieldViewModel('0'));
+			row.push(new FieldViewModel(index, i, ' '));
 		}
 		return row;
 	}
 	for(var i = 0; i<3; i++) {
-		self.rows.push({cols: makeRow()});
+		self.rows.push({cols: makeRow(i)});
 	}
 }
 
-function FieldViewModel(state) {
+function FieldViewModel(x,y,state) {
 	var self = this;
-	// self.x = x;
-	// self.y = y;
+	self.x = x;
+	self.y = y;
 	self.state = ko.observable(state || ' ');
 }
 
