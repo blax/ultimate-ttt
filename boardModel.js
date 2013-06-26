7ec9fc6c7e946430cb9abcef2ed461b85520e054
Arek Flinik <aflinik@gmail.com>
2013-06-26 17:26:05 +0200

    determining winner of a big board

--- boardModel.js
+++ boardModel.js
@@ -1,3 +1,31 @@
+function ticTacToeWinner(board) {
+	var diagACount = {'O': 0, 'X': 0};
+	var diagBCount = {'O': 0, 'X': 0};
+	for(var i = 0; i<3; i++){
+		var colCount = {'O': 0, 'X': 0};
+		var rowCount = {'O': 0, 'X': 0};
+		for(var j = 0; j<3; j++) {
+			colCount[board[i][j]]++;
+			rowCount[board[j][i]]++;
+		}
+		if(colCount['O'] == 3 || rowCount['O'] == 3){
+				return 'O';
+		}
+		if(colCount['X'] == 3 || rowCount['X'] == 3){
+			return 'X';
+		}
+		diagACount[board[i][i]]++;
+		diagBCount[board[i][2-i]]++;
+	}
+	if(diagACount['O'] == 3 || diagBCount['O'] == 3){
+			return 'O';
+	}
+	if(diagACount['X'] == 3 || diagBCount['X'] == 3){
+		return 'X';
+	}
+	return false;
+}
+
 function BigBoardModel() {
 	var self = this;
 
@@ -15,22 +43,37 @@ function BigBoardModel() {
 
 	self.makeMove = function(field) {
 		var allowedBoard = !self.nextSmallBoard() || self.nextSmallBoard() == field.parent;
+		var next = self.big_rows[field.x].big_cols[field.y];
+		if(self.winner()){
+			return false;
+		}
 		if(allowedBoard && field.state() == ' '){
 			field.state(self.nextMove());
 			self.switchMove();
 			self.lastMove(field);
-			var next = self.big_rows[field.x].big_cols[field.y];
 			self.nextSmallBoard(next);
 		}
 		if(allowedBoard && field.parent.isFilled()){
 			self.switchMove();
 			self.lastMove(null);
-			var next = self.big_rows[field.x].big_cols[field.y];
 			self.nextSmallBoard(next);
 		}
 
 	};
-	
+
+	self.winner = ko.computed(function() {
+		var board = [];
+		for(var i = 0; i < 3; i++){
+			var row = [];
+			for(var j = 0; j < 3; j++) {
+				row.push(self.big_rows[i].big_cols[j].winner());
+			}
+			board.push(row);
+		}
+		console.log(board);
+		return ticTacToeWinner(board);
+	}, self, {deferEvaluation: true});
+
 	// big board setup
 	self.big_rows = [];
 	var makeRow = function() {
